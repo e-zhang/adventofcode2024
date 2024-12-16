7dfd5c76a8b08d93ae332af0e8a22a7a38d82b19
ericz <eczhang530@gmail.com>
2024-12-16 08:58:01 -0800

    day16 with bfs

--- 16/main.go
+++ 16/main.go
@@ -58,33 +58,11 @@ func main() {
 	}
 
 	// init := Coord{-1, -1}
-	seen := map[key]int{}
-	remaining := map[key]cache{}
-	chain := path{-1, nil}
-	score, _ := Maze(start, E, end, []Coord{}, grid, 0, seen, remaining, &chain)
-	fmt.Println(score)
-
-	// path := map[Coord]struct{}{end: struct{}{}}
-	// backtrack(end, init, chain, path)
-	debug(len(chain.tiles) + 1)
-	print(grid, chain.tiles)
-}
-
-func backtrack(pos, end Coord, chain map[Coord]map[Coord]struct{}, path map[Coord]struct{}) {
-	if pos == end {
-		return
-	}
-
-	prevs := chain[pos]
-	debug(pos, prevs)
-	for p := range prevs {
-		if _, ok := path[p]; ok {
-			continue
-		}
-		path[p] = struct{}{}
-		backtrack(p, end, chain, path)
-	}
-
+	// seen := map[key]int{}
+	// chain := path{-1, nil}
+	// score, _ := MazeDFS(start, E, end, []Coord{}, grid, 0, seen, &chain)
+	score, tiles := MazeBFS(start, E, end, grid)
+	fmt.Println(score, tiles)
 }
 
 func print(grid []string, path map[Coord]struct{}) {
@@ -138,7 +116,7 @@ type cache struct {
 	path  []Coord
 }
 
-func Maze(pos, dir, end Coord, prevs []Coord, grid []string, score int, seen map[key]int, remaining map[key]cache, chain *path) (int, []Coord) {
+func MazeDFS(pos, dir, end Coord, prevs []Coord, grid []string, score int, seen map[key]int, chain *path) (int, []Coord) {
 	k := key{pos, dir}
 	v, ok := seen[k]
 	if ok {
@@ -162,19 +140,15 @@ func Maze(pos, dir, end Coord, prevs []Coord, grid []string, score int, seen map
 		return score, prevs
 	}
 
-	if v, ok := remaining[k]; ok {
-		return Maze(end, dir, end, append(prevs, v.path...), grid, score+v.score, seen, remaining, chain)
-	}
-
 	options := []cache{}
 
-	cw, cwPath := Maze(pos, dir.Rotate(true), end, prevs, grid, score+1000, seen, remaining, chain)
+	cw, cwPath := MazeDFS(pos, dir.Rotate(true), end, prevs, grid, score+1000, seen, chain)
 	options = append(options, cache{cw, cwPath})
-	ccw, ccwPath := Maze(pos, dir.Rotate(false), end, prevs, grid, score+1000, seen, remaining, chain)
+	ccw, ccwPath := MazeDFS(pos, dir.Rotate(false), end, prevs, grid, score+1000, seen, chain)
 	options = append(options, cache{ccw, ccwPath})
 	next := pos.Add(dir)
 	if grid[next.row][next.col] != '#' {
-		moveScore, movePath := Maze(next, dir, end, append(prevs, pos), grid, score+1, seen, remaining, chain)
+		moveScore, movePath := MazeDFS(next, dir, end, append(prevs, pos), grid, score+1, seen, chain)
 		options = append(options, cache{moveScore, movePath})
 	}
 
@@ -185,10 +159,6 @@ func Maze(pos, dir, end Coord, prevs []Coord, grid []string, score int, seen map
 		}
 	}
 
-	// debug(pos, best.score, score, options)
-	if best.score >= score {
-		// remaining[k] = cache{best.score - score, best.path}
-	}
 	return best.score, best.path
 }
 
@@ -207,4 +177,61 @@ func (c Coord) Rotate(clockwise bool) Coord {
 	} else {
 		return Coord{c.col, -c.row}
 	}
+}
+
+type node struct {
+	pos   Coord
+	dir   Coord
+	score int
+	path  []Coord
+}
+
+func MazeBFS(pos, dir, end Coord, grid []string) (int, int) {
+	q := []node{{pos, dir, 0, nil}}
+	seen := map[key]int{}
+
+	best := 0
+	tiles := map[Coord]struct{}{}
+
+	for len(q) > 0 {
+		curr := q[0]
+		q = q[1:]
+
+		if curr.pos == end {
+			if best > 0 && curr.score > best {
+				continue
+			}
+			if best == 0 || curr.score < best {
+				best = curr.score
+				tiles = map[Coord]struct{}{end: struct{}{}}
+			}
+
+			for _, c := range curr.path {
+				tiles[c] = struct{}{}
+			}
+			continue
+		}
+
+		k := key{curr.pos, curr.dir}
+		if v, ok := seen[k]; ok && curr.score > v {
+			continue
+		}
+		seen[k] = curr.score
+		path := make([]Coord, len(curr.path))
+		copy(path, curr.path)
+		for _, n := range []node{
+			{curr.pos.Add(curr.dir), curr.dir, curr.score + 1, append(path, curr.pos)},
+			{curr.pos, curr.dir.Rotate(true), curr.score + 1000, path},
+			{curr.pos, curr.dir.Rotate(false), curr.score + 1000, path},
+		} {
+			if grid[n.pos.row][n.pos.col] == '#' {
+				continue
+			}
+			q = append(q, n)
+		}
+	}
+
+	debug(best, len(tiles))
+	print(grid, tiles)
+	return best, len(tiles)
 }
